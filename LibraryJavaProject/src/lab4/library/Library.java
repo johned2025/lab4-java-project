5fb36d57114f354524f3b6fd24be7b2fb4481a56
johned2025 <johned9321@gmail.com>
2024-11-10 21:46:17 -0800

    Update Library.java

--- LibraryJavaProject/src/lab4/library/Library.java
+++ LibraryJavaProject/src/lab4/library/Library.java
@@ -19,7 +19,7 @@ public class Library {
 	// DONE: implement functionality of Member class
 	public Library() {
 		catalog = new ArrayList<>();
-		members = new ArrayList<>();
+		members= new ArrayList<>();
 	}
 	public void addMembers(Member member) {
 		members.add(member);
@@ -29,11 +29,51 @@ public class Library {
 	}
 	public void addBook(Book book) {
 		catalog.add(book);
+		System.out.println(book.getTitle()+ " was added to library catalog");
 	}
 	public void addBook(String bookTitle) {
 		
 		catalog.add(new Book(bookTitle));
+		System.out.println(bookTitle+ " was added to library catalog");
 	}
-	
+	// retrieves a member from members' list
+	// or  creates a member and add it to members' list
+	public Member getMember(String name) {
+		for(Member m : members) {
+			if(m.getName() == name) return m;
+		}
+		Member m = new Member(name);
+		this.addMembers(m);
+		return m;
+	}
+	public Book getBook(String BookName) {
+		for(Book b : catalog) {
+			if(b.getTitle() == BookName) return b;
+		}
+		Book b = new Book(BookName);
+		this.addBook(b);
+		return b;
+	}
+	public void showAllBooks() {
+		System.out.println("** Library Catalog **");
+		for(Book b: catalog) {
+			System.out.println(b.getTitle());
+		}
+	}
+	public void showAllMembers() {
+		System.out.println("** Library Members **");
+		for(Member m: members) {
+			System.out.println(m.getName());
+		}
+	}
+	public void showAvailable() {
+		System.out.println("** Library Available Books **");
+		for(Book b: catalog) {
+			if(b.getAvailability()) {
+				System.out.println(b.getTitle());
+			}
+			
+		}
+	}
 	
 }
