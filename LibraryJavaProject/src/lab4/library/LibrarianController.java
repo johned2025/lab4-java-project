5321fd43674fa19bb8416318c566b159e71c133d
johned2025 <johned9321@gmail.com>
2024-11-10 21:48:49 -0800

    Update LibrarianController.java

--- LibraryJavaProject/src/lab4/library/LibrarianController.java
+++ LibraryJavaProject/src/lab4/library/LibrarianController.java
@@ -7,13 +7,42 @@ public class LibrarianController {
 	// Delegates tasks to the appropriate objects.
 	
 	// TODO: implement functionality of Member class
-	Library library = new Library();
+	Library library;
+	public LibrarianController() {
+		library = new Library();
+	}
 	
-	public void addBookToCatalog(Book book) { //adds an existing book
+	
+	public void addBookToCatalog(Book book) { //adds an existing book object
 		library.addBook(book);
 	}
 	public void addBookToCatalog(String bookTitle) { //creates and adds a new book
 		library.addBook(bookTitle);
+	}
+	public void addMember(String Name) {
+		library.addMembers(Name);
+		}
+	public void borrowBook(String bookTitle, String memberName) {
+		Member m = library.getMember(memberName);
+		Book b = library.getBook(bookTitle);
+		if(m.borrowBook(b)) { // borrowBook return true if book was successfully borrowed
+			b.updateStatus(); // sets the book to unavailable
+		}
+	}
+	public void returnBook(String bookTitle, String memberName) {
+		Member m = library.getMember(memberName);
+		Book b = library.getBook(bookTitle);
+		b.updateStatus();
+		m.returnBook(b);
+	}
+	public void showCatalog() {
+		library.showAllBooks();
+	}
+	public void showMembers() {
+		library.showAllMembers();
+	}
+	public void showAvailableBoooks() {
+		library.showAvailable();
 	}
 	
 
